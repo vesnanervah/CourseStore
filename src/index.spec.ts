04440dc6e8d848b32bdc1b76f43ef110627421ad
Ilya Fakhrutdinov <32900366+f4hr@users.noreply.github.com>
2023-08-07 23:15:08 +0500

    build: add `jest` config
    - add jest config
    - add jest npm script
    - add husky pre-commit test script
    - add test coverage script

---
+++ src/index.spec.ts
@@ -0,0 +1,8 @@
+// TODO: replace with real tests
+describe('sum', () => {
+  describe('when add 1 and 2', () => {
+    it('should return 3', () => {
+      expect(1 + 2).toBe(3);
+    });
+  });
+});
