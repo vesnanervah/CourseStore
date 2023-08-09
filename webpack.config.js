5f8e84eade446b77d3267d3832cc956f67a28db3
Tamara Fedyanova <112993598+toma-fedyanova@users.noreply.github.com>
2023-08-10 02:32:02 +1000

    feat: add html, scss, plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 
 module.exports = (env) => {
   return {
@@ -8,11 +10,22 @@ module.exports = (env) => {
       filename: 'main.js',
       path: path.resolve(__dirname, 'dist'),
     },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, 'src', 'index.html'),
+        filename: 'index.html',
+      }),
+      new FaviconsWebpackPlugin(path.join(__dirname, 'src', 'assets', 'favicon.ico')),
+    ],
     module: {
       rules: [
         {
           test: /\.s[ac]ss$/i,
           use: ['style-loader', 'css-loader', 'sass-loader'],
+        },
+        {
+          test: /\.ts$/i,
+          use: 'ts-loader',
         },
       ],
     },
