a20d2ef0483958ab80678bd130cd9ef3dfd88897
Tamara Fedyanova <112993598+toma-fedyanova@users.noreply.github.com>
2023-08-10 11:31:50 +1000

    fix config

--- webpack.config.js
+++ webpack.config.js
@@ -10,12 +10,15 @@ module.exports = (env) => {
       filename: 'main.js',
       path: path.resolve(__dirname, 'dist'),
     },
+    resolve: {
+      extensions: ['.ts', '.js', '.json'],
+    },
     plugins: [
       new HtmlWebpackPlugin({
         template: path.join(__dirname, 'src', 'index.html'),
         filename: 'index.html',
       }),
-      new FaviconsWebpackPlugin(path.join(__dirname, 'src', 'assets', 'favicon.ico')),
+      new FaviconsWebpackPlugin(path.join(__dirname, './src/assets/favicon.ico')), //'src', 'assets',
     ],
     module: {
       rules: [
@@ -26,6 +29,13 @@ module.exports = (env) => {
         {
           test: /\.ts$/i,
           use: 'ts-loader',
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+          generator: {
+            filename: 'assets/[name][ext]',
+          },
         },
       ],
     },
