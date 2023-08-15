9305f1f5d2103c20b1c0bd3910ff3c64dfe0f0ab
Ilya Fakhrutdinov <ilyaf4hr@gmail.com>
2023-08-15 22:55:08 +0500

    chore: add app container styles

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,19 +1,29 @@
+import './app.scss';
 import { MainPage } from '../pages';
 
 export class App {
-  private rootElement: HTMLElement;
+  private appContainer: HTMLElement;
 
   constructor(root: HTMLElement) {
-    this.rootElement = root;
+    this.appContainer = this.createAppContainer();
+
+    root.append(this.appContainer);
   }
 
   public start(): void {
     this.renderMainPage();
   }
 
+  private createAppContainer(): HTMLElement {
+    const container = document.createElement('div');
+    container.classList.add('app');
+
+    return container;
+  }
+
   private renderMainPage(): void {
     const page = new MainPage();
 
-    this.rootElement.append(page.getHtmlElement());
+    this.appContainer.append(page.getHtmlElement());
   }
 }
