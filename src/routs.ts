d2eb991412a33199ced411f7b209a15a2a333d0c
Ilya Fakhrutdinov <ilyaf4hr@gmail.com>
2023-08-19 20:47:41 +0500

    chore: remove routs.ts

--- src/routs.ts
+++
@@ -1,7 +0,0 @@
-export const routes = {
-  main: () => '/#/',
-  login: () => '/#/login',
-  signup: () => '/#/signup',
-  category: (slug: string) => `/#/categories/${slug}`,
-  about: () => '/#/about',
-};
