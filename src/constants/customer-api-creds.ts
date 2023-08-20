6c5364d792ffd198e6bdaeb9e40d78e8506e9d7b
Ilya Fakhrutdinov <32900366+f4hr@users.noreply.github.com>
2023-08-20 17:56:00 +0500

    chore: add dotenv (#18)

--- src/constants/customer-api-creds.ts
+++ src/constants/customer-api-creds.ts
@@ -1,34 +1,65 @@
 import { EcommerceApiCreds } from '../types/ecommerce-api-creds';
 
+const ENV_KEYS = [
+  'API_HOST',
+  'AUTH_HOST',
+  'API_PROJECT_KEY',
+  'API_CLIENT_ID',
+  'API_SECRET',
+] as const;
+
+const API_HOST = process.env.API_HOST || '';
+const AUTH_HOST = process.env.AUTH_HOST || '';
+const API_PROJECT_KEY = process.env.API_PROJECT_KEY || '';
+const API_CLIENT_ID = process.env.API_CLIENT_ID || '';
+const API_SECRET = process.env.API_SECRET || '';
+
+type EnvVars = { [key in (typeof ENV_KEYS)[number]]: string };
+
+const SCOPE = [
+  'manage_my_business_units',
+  'view_product_selections',
+  'introspect_oauth_tokens',
+  'view_products',
+  'manage_my_orders',
+  'manage_my_payments',
+  'view_shopping_lists',
+  'view_quotes',
+  'manage_my_quote_requests',
+  'create_anonymous_token',
+  'manage_my_quotes',
+  'view_messages',
+  'view_payments',
+  'manage_my_profile',
+  'view_stores',
+  'view_types',
+  'view_orders',
+  'view_categories',
+  'manage_my_shopping_lists',
+  'view_published_products',
+  'view_customers',
+] as const;
+
+const ENV_VARS: EnvVars = {
+  API_HOST,
+  AUTH_HOST,
+  API_PROJECT_KEY,
+  API_CLIENT_ID,
+  API_SECRET,
+};
+
+const undefinedEnvVar = Object.entries(ENV_VARS).find(([, val]) => !val);
+if (undefinedEnvVar) {
+  throw new Error(`'${undefinedEnvVar[0]}' environment variable is undefined`);
+}
+
 const CUSTOMER_API_CREDS: EcommerceApiCreds = {
-  project_key: 'coursestore',
-  client_id: 'Q53bj2ohY8r6GnT_2ObtsHYE',
-  secret: 'aRPWZhE_PkqrxjfmpYElTXidCjSlkT4i',
-  scope: [
-    'manage_my_business_units:coursestore',
-    'view_product_selections:coursestore',
-    'introspect_oauth_tokens:coursestore',
-    'view_products:coursestore',
-    'manage_my_orders:coursestore',
-    'manage_my_payments:coursestore',
-    'view_shopping_lists:coursestore',
-    'view_quotes:coursestore',
-    'manage_my_quote_requests:coursestore',
-    'create_anonymous_token:coursestore',
-    'manage_my_quotes:coursestore',
-    'view_messages:coursestore',
-    'view_payments:coursestore',
-    'manage_my_profile:coursestore',
-    'view_stores:coursestore',
-    'view_types:coursestore',
-    'view_orders:coursestore',
-    'view_categories:coursestore',
-    'manage_my_shopping_lists:coursestore',
-    'view_published_products:coursestore',
-    'view_customers:coursestore',
-  ],
-  API_Host: 'https://api.us-central1.gcp.commercetools.com',
-  Auth_Host: 'https://auth.us-central1.gcp.commercetools.com',
+  project_key: ENV_VARS.API_PROJECT_KEY,
+  client_id: ENV_VARS.API_CLIENT_ID,
+  secret: ENV_VARS.API_SECRET,
+  scope: SCOPE.map((scope) => `${scope}:${ENV_VARS.API_PROJECT_KEY}`),
+  API_Host: ENV_VARS.API_HOST,
+  Auth_Host: ENV_VARS.AUTH_HOST,
 };
 
 export { CUSTOMER_API_CREDS };
