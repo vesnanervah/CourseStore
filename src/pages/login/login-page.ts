c66b4a45a58e41801b97959f535e207010a7aee3
Ilya Fakhrutdinov <ilyaf4hr@gmail.com>
2023-08-21 23:21:01 +0500

    chore: redirect to main page if logged in

--- src/pages/login/login-page.ts
+++ src/pages/login/login-page.ts
@@ -1,18 +1,33 @@
 import { BaseView } from '../../features/ui';
 import { MainLayout } from '../../features/layouts';
+import { AppRouter } from '../../features/router';
 import LoginView from './login';
+import { routes } from '../../routes';
+import Auth from '../../features/auth/auth';
 
-export class LoginPage extends BaseView {
+import { AuthListener } from '../../types/auth';
+
+export class LoginPage extends BaseView implements AuthListener {
+  private router = AppRouter.getInstance();
   private loginForm = new LoginView();
 
   constructor() {
     super();
     this.createElement();
+    Auth.subscribe(this);
   }
 
   private createElement(): void {
     const layout = new MainLayout(this.loginForm.getHtmlElement());
 
     this.htmlElement = layout.getHtmlElement();
+  }
+
+  listenLogout(): void {
+    //
+  }
+
+  listenLogin(): void {
+    this.router.navigate(routes.main());
   }
 }
