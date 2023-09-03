ef569553294d1df6d65d5deb4974920f03c69062
Nervniy <c.zwerew2012@gmail.com>
2023-09-03 15:52:39 +0300

    delete mocks in indext.ts

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,5 @@
 import './style.scss';
-import EcommerceClient from './features/commerce/BuildClient';
-import ProductView from './pages/product/product';
-/*import { App } from './app/app';
+import { App } from './app/app';
 
 const app: App = new App(document.body);
-app.start();*/
-const pv = new ProductView();
-
-async function getProd() {
-  EcommerceClient.stockRootPrepare();
-  const randomId = (await EcommerceClient.getProducts()).body.results[0].id;
-  const view = pv.getHtmlElement();
-  await pv.updateProductPage(randomId);
-  document.body.appendChild(view);
-}
-
-getProd();
+app.start();
