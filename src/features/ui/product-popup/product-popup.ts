00cb2fca798a51c6461516c1f2636e9a9dea4211
Nervniy <c.zwerew2012@gmail.com>
2023-09-03 18:34:07 +0300

    feat: add product popup

--- src/features/ui/product-popup/product-popup.ts
+++ src/features/ui/product-popup/product-popup.ts
@@ -1,29 +1,28 @@
 import { Images } from '../../../types/product';
 import { BaseView } from '../base-view';
-import ProductPopup from '../product-popup/product-popup';
-import './product-slider.scss';
-
-export default class ProductSlider extends BaseView {
-  private imageOne: HTMLElement;
-  private imageTwo: HTMLElement;
+import './product-popup.scss';
+//  TODO: refactor copypast to separate class
+export default class ProductPopup extends BaseView {
+  private imgOne: HTMLElement;
+  private imgTwo: HTMLElement;
   private btnOne: HTMLElement;
   private btnTwo: HTMLElement;
-  private images: HTMLElement;
   private btns: HTMLElement;
   private content: HTMLElement;
-  private popup = new ProductPopup();
+  private images: HTMLElement;
 
   constructor() {
     super();
     this.htmlElement = document.createElement('div');
+    this.hide();
     this.content = document.createElement('div');
     this.content.className = 'slider-content';
     this.images = document.createElement('div');
-    this.imageOne = document.createElement('div');
-    this.imageTwo = document.createElement('div');
-    this.imageOne.className = 'product__image product__image-one';
-    this.imageTwo.className = 'product__image product__image-two';
-    this.images.append(this.imageOne, this.imageTwo);
+    this.imgOne = document.createElement('div');
+    this.imgTwo = document.createElement('div');
+    this.imgOne.className = 'product__image product__image-one';
+    this.imgTwo.className = 'product__image product__image-two';
+    this.images.append(this.imgOne, this.imgTwo);
     this.images.className = 'product__images';
     this.btns = document.createElement('div');
     this.btnOne = document.createElement('span');
@@ -33,27 +32,19 @@ export default class ProductSlider extends BaseView {
     this.btns.append(this.btnOne, this.btnTwo);
     this.btns.className = 'product__sliderbtns';
     this.content.append(this.images, this.btns);
-    this.htmlElement.append(this.content, this.popup.getHtmlElement());
-    this.htmlElement.className = 'product__slider';
+    this.htmlElement.append(this.content);
+    this.htmlElement.className = 'product__popup';
     this.btnOne.addEventListener('click', () => this.handleLeftClick());
     this.btnTwo.addEventListener('click', () => this.handleRightClick());
-    this.images.addEventListener('click', () => this.showPopup());
-  }
-
-  public setImages(images: Images): void {
-    this.imageOne.style.backgroundImage = `url(${images[0].url})`;
-    if (images[1]) {
-      this.imageTwo.style.backgroundImage = `url(${images[1].url})`;
-    }
-    this.popup.setImages(images);
+    this.images.addEventListener('click', () => this.hide());
   }
 
   private handleLeftClick() {
     if (this.btnOne.classList.contains('inactive-btn')) {
       return;
     }
-    this.imageOne.style.order = '0';
-    this.imageTwo.style.order = '1';
+    this.imgOne.style.order = '0';
+    this.imgTwo.style.order = '1';
     this.btnOne.classList.add('inactive-btn');
     this.btnTwo.classList.remove('inactive-btn');
   }
@@ -62,13 +53,27 @@ export default class ProductSlider extends BaseView {
     if (this.btnTwo.classList.contains('inactive-btn')) {
       return;
     }
-    this.imageOne.style.order = '2';
-    this.imageTwo.style.order = '0';
+    this.imgOne.style.order = '2';
+    this.imgTwo.style.order = '0';
     this.btnTwo.classList.add('inactive-btn');
     this.btnOne.classList.remove('inactive-btn');
   }
 
-  private showPopup(): void {
-    this.popup.show();
+  public setImages(images: Images): void {
+    this.imgOne.style.backgroundImage = `url(${images[0].url})`;
+    if (images[1]) {
+      this.imgTwo.style.backgroundImage = `url(${images[1].url})`;
+    }
+  }
+
+  public show(): void {
+    (this.htmlElement as HTMLElement).classList.remove('hidden');
+    (this.htmlElement as HTMLElement).style.top = `${window.pageYOffset}px`;
+    document.body.style.overflow = 'hidden';
+  }
+
+  public hide(): void {
+    (this.htmlElement as HTMLElement).classList.add('hidden');
+    document.body.style.overflow = 'auto';
   }
 }
