d818f1b367590d2889eeee816ed34061f459ceb1
Nervniy <c.zwerew2012@gmail.com>
2023-09-03 15:50:47 +0300

    feat: add slider btns click handlers

--- src/features/ui/product-slider/product-slider.ts
+++ src/features/ui/product-slider/product-slider.ts
@@ -7,7 +7,7 @@ export default class ProductSlider extends BaseView {
   private imageTwo: HTMLElement;
   private btnOne: HTMLElement;
   private btnTwo: HTMLElement;
-  private pos = 1;
+
   constructor() {
     super();
     this.htmlElement = document.createElement('div');
@@ -21,12 +21,14 @@ export default class ProductSlider extends BaseView {
     const btns = document.createElement('div');
     this.btnOne = document.createElement('span');
     this.btnTwo = document.createElement('span');
-    this.btnOne.className = 'product__sliderbtn product__sliderbtn-one';
+    this.btnOne.className = 'product__sliderbtn product__sliderbtn-one inactive-btn';
     this.btnTwo.className = 'product__sliderbtn product__sliderbtn-two';
     btns.append(this.btnOne, this.btnTwo);
     btns.className = 'product__sliderbtns';
     this.htmlElement.append(images, btns);
     this.htmlElement.className = 'product__slider';
+    this.btnOne.addEventListener('click', () => this.handleLeftClick());
+    this.btnTwo.addEventListener('click', () => this.handleRightClick());
   }
 
   public setImages(images: Images): void {
@@ -34,5 +36,25 @@ export default class ProductSlider extends BaseView {
     if (images[1]) {
       this.imageTwo.style.backgroundImage = `url(${images[1].url})`;
     }
+  }
+
+  private handleLeftClick() {
+    if (this.btnOne.classList.contains('inactive-btn')) {
+      return;
+    }
+    this.imageOne.style.order = '0';
+    this.imageTwo.style.order = '1';
+    this.btnOne.classList.add('inactive-btn');
+    this.btnTwo.classList.remove('inactive-btn');
+  }
+
+  private handleRightClick() {
+    if (this.btnTwo.classList.contains('inactive-btn')) {
+      return;
+    }
+    this.imageOne.style.order = '2';
+    this.imageTwo.style.order = '0';
+    this.btnTwo.classList.add('inactive-btn');
+    this.btnOne.classList.remove('inactive-btn');
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,18 @@
 import './style.scss';
-import { App } from './app/app';
+import EcommerceClient from './features/commerce/BuildClient';
+import ProductView from './pages/product/product';
+/*import { App } from './app/app';
 
 const app: App = new App(document.body);
-app.start();
+app.start();*/
+const pv = new ProductView();
+
+async function getProd() {
+  EcommerceClient.stockRootPrepare();
+  const randomId = (await EcommerceClient.getProducts()).body.results[0].id;
+  const view = pv.getHtmlElement();
+  await pv.updateProductPage(randomId);
+  document.body.appendChild(view);
+}
+
+getProd();
