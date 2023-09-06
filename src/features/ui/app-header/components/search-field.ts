6115e7357649da6acd4d51384665a18afe2555aa
Kirill <106981545+vesnanervah@users.noreply.github.com>
2023-09-06 14:16:28 +0300

    Search product (#42)
    * feat: add search
    
    * feat: add rout after search result click

--- src/features/ui/app-header/components/search-field.ts
+++ src/features/ui/app-header/components/search-field.ts
@@ -4,39 +4,40 @@ import { TextField } from '../../text-field';
 import { Icon } from '../../icon';
 import { IconButton } from '../../icon-button';
 import searchIcon from '../../../../assets/images/icons/search.svg';
+import EcommerceClient from '../../../commerce/BuildClient';
+import SearchResults from './search-results';
 
 export class SearchField extends BaseView<HTMLElement> {
+  private searchResults = new SearchResults();
+
   constructor() {
     super();
-
     this.createElement();
+    this.htmlElement?.appendChild(this.searchResults.getHtmlElement());
   }
 
   private createElement(): void {
     const container = document.createElement('div');
     container.classList.add('search-field');
-
     const searchFieldIcon = new Icon({
       id: searchIcon.id,
       viewBox: searchIcon.viewBox,
     });
     const searchFieldIconElement = searchFieldIcon.getHtmlElement();
     searchFieldIconElement.classList.add('text-field__icon');
-
     const searchField = new TextField({
       placeholder: 'Поиск',
       icon: searchFieldIconElement,
     });
+    const sfInput = searchField.getInputElem();
+    sfInput.addEventListener('keyup', () => this.handleInput(searchField.getInputElem()));
     const searchFieldElement = searchField.getHtmlElement();
     searchFieldElement.classList.add('search-field__input');
     container.append(searchFieldElement);
-
-    // TODO: replace with IconButton and handle click
     const searchToggleIcon = new Icon({
       id: searchIcon.id,
       viewBox: searchIcon.viewBox,
     });
-
     const searchToggle = new IconButton({
       type: 'button',
       icon: searchToggleIcon.getHtmlElement() as HTMLElement,
@@ -46,5 +47,24 @@ export class SearchField extends BaseView<HTMLElement> {
     container.append(searchToggleElement);
 
     this.htmlElement = container;
+  }
+
+  private handleInput(field: HTMLInputElement) {
+    const typed = field.value;
+    if (typed.length === 0) {
+      this.searchResults.hide();
+      return;
+    }
+    this.searchQuery(typed);
+  }
+
+  private async searchQuery(query: string) {
+    const prods = await EcommerceClient.getProductByQuery(5, query);
+    console.log(prods);
+    this.searchResults.setContent(prods);
+    (this.searchResults.getHtmlElement() as HTMLElement).style.top = `${
+      (this.htmlElement as HTMLElement).getBoundingClientRect().y + 40
+    }px`;
+    this.searchResults.reveal();
   }
 }
